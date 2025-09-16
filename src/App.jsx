9d8cb19e9b35c1c3d70f4d4ee728708d54dbacff
Johnbyeon <ghks0115@gmail.com>
2025-09-16 14:52:56 +0900

    신발 사진

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,54 @@
 import { useState } from 'react'
 import './App.css'
 import AppNavBar from './AppNavBar'
+//assets 폴더 내의 이미지 사용법 ->import해서 사용해야함
+import bg_png from './assets/images/bg.png';
 
+import img_2 from './assets/images/shoes2.jpg';
+
+import { Container, Col, Row } from 'react-bootstrap';
+
+import data from './data/data.js'
 
 function App() {
-
+  //상품 정보를 받는 프로덕트 스테이트를 만든다 
+  const [product,setProduct] = useState(data);
+  console.log(product);
   return (
     <>
-        <AppNavBar />
+      {/* 네비게이션바 */}
+      <AppNavBar />
+
+      {/* 메인 대문 사진 영역 */}
+      <div className='main-bg'
+      style={{backgroundImage:`url('${bg_png}')`
+      }}
+      />
+
+      {/* */}
+    <Container>
+      <Row>
+        <Col className='text-center'>
+          <img src="http://zzzmini.github.io/images/shoes1.jpg"
+          width="80%" />
+          <h4>상품명</h4>
+          <p>상품설명</p>
+        </Col>
+        <Col className='text-center'>
+          <img src= {img_2}
+          width="80%" />
+          <h4>상품명</h4>
+          <p>상품설명</p>
+        </Col>
+        <Col className='text-center'>
+          <img src= "/images/shoes3.jpg"
+          width="80%" />
+          <h4>상품명</h4>
+          <p>상품설명</p>
+        </Col>
+      </Row>
+    </Container>
+
     </>
   )
 }
