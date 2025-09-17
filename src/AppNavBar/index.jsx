3d08baef96b162b5de52b476b16182d660c3c188
Johnbyeon <ghks0115@gmail.com>
2025-09-17 10:25:14 +0900

    no message

--- src/AppNavBar/index.jsx
+++ src/AppNavBar/index.jsx
@@ -15,8 +15,8 @@ function AppNavBar() {
                     <Nav className="me-auto">
                         <Nav.Link onClick={()=>{navigate('/')}}>Home</Nav.Link>
                         <Nav.Link onClick={()=>{navigate('/detail')}}>Detail</Nav.Link>
-                        <Nav.Link onClick={()=>{navigate('/cart')}}>Cart</Nav.Link>
-                        <Nav.Link onClick={()=>{navigate('/about')}}>About</Nav.Link>                        <NavDropdown id="navbarScrollingDropdown" title="About">
+                        <Nav.Link onClick={()=>{navigate('/cart')}}>Cart</Nav.Link>                      
+                        <NavDropdown id="navbarScrollingDropdown" title="About">
                             <NavDropdown.Item onClick={()=>{navigate('/about/member')}}>member</NavDropdown.Item>
                             <NavDropdown.Item onClick={()=>{navigate('/about/location')}}>location</NavDropdown.Item>
                         </NavDropdown>
